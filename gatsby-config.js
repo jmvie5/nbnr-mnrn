2ef7c35e3dd980e815608c17240dc9941361278c
jmvie5 <jeanmichel.viel@gmail.com>
2023-10-08 09:11:14 -0400

    migrate to netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,33 +15,34 @@ module.exports = {
       },
       {
         name: "Gallery",
-        link: "/gallery"
+        link: "/gallery",
       },
       {
         name: "Videos",
-        link: "/videos"
+        link: "/videos",
       },
       {
         name: "Ceremonial Music",
-        link: "/ceremonial-music"
+        link: "/ceremonial-music",
       },
       {
         name: "Concerts",
-        link: "/concerts"
+        link: "/concerts",
       },
       {
         name: "Jobs",
-        link: "/jobs"
-      }
+        link: "/jobs",
+      },
     ],
   },
   plugins: [
+    "gatsby-plugin-netlify",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `gallery`,
         path: `${__dirname}/gallery`,
-      }
+      },
     },
     "gatsby-plugin-image",
     {
@@ -53,21 +54,21 @@ module.exports = {
           quality: 90,
           breakpoints: [750, 1080, 1366, 1920],
           backgroundColor: `transparent`,
-        }
-      }
+        },
+      },
     },
     "gatsby-plugin-mdx",
     "gatsby-plugin-react-helmet",
     "gatsby-transformer-sharp",
-    'gatsby-plugin-postcss',
+    "gatsby-plugin-postcss",
     `gatsby-plugin-sitemap`,
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: "gatsby-plugin-robots-txt",
       options: {
-        host: 'https://nbnr.ca',
-        sitemap: 'https://nbnr.ca/sitemap-index.xml',
-        policy: [{userAgent: '*', allow: '/'}]
-      }
+        host: "https://nbnr.ca",
+        sitemap: "https://nbnr.ca/sitemap-index.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
     },
     {
       resolve: `gatsby-plugin-intl`,
@@ -75,8 +76,8 @@ module.exports = {
         path: `${__dirname}/src/intl`,
         languages: [`en`, `fr`],
         defaultLanguage: `en`,
-        redirect: true
-      }
+        redirect: true,
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -87,8 +88,8 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#276688`,
         display: `standalone`,
-        icon: `src/images/icon.png`
-      }
+        icon: `src/images/icon.png`,
+      },
     },
   ],
-}
+};
